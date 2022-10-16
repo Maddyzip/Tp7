fabc60f9a719993e5a9d65caec183e7a6cbb76e2
maddyzip <maddyszip@gmail.com>
2022-10-16 23:23:20 +0000

    add all html files

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 // JavaScript for TP7
 
+
 // function to load a file from the URL "fromFile" into the object identified by "whereTo"
 function loadFileInto(fromFile, whereTo) {
 
@@ -12,7 +13,7 @@ function loadFileInto(fromFile, whereTo) {
 	// provides code to do something in response to the AJAX request
 	ajax.onreadystatechange = function() {
 			if ((this.readyState == 4) && (this.status == 200)) {
-				document.querySelector(whereTo).innerHTML += this.responseText;
+				document.querySelector(whereTo).innerHTML = this.responseText;
 				
 			} else if ((this.readyState == 4) && (this.status != 200)) {
 				console.log("Error: " + this.responseText);
@@ -26,40 +27,56 @@ function loadFileInto(fromFile, whereTo) {
 }
 
 
-
-
-
-/*window.onload = function() {
+//new recipe object 
+function Recipe( recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
 	
-	loadFileInto("ingredients.html", "#ingredients ul");
-	loadFileInto("equipment.html", "#equipment ul");
-	loadFileInto("directions.html", "#directions ol");
+	this.recipeName = recipeName;
+	this.contributor = contributorName;
+	this.imageURL = imageURL;
+	this.ingredients = ingredientsURL;
+	this.equipment = equipmentURL;
+	this.directions = directionsURL;
 	
-	
-	
-	document.querySelector("#titleBanner h1").classList.add("tp");
-	
-	document.querySelector("#titleBanner h1").onclick = function() {
-		this.classList.toggle("black");
-	}
-	
-	document.querySelector("#recipeColumns #ingredients").onclick = function() {
-		this.classList.toggle("tpShow");
-	}
-	
-	document.querySelector("#recipeColumns #equipment").onclick = function() {
-		this.classList.toggle("tpShow");
-	}
+	this.displayRecipe = function() {
 		
-	document.querySelector("#recipeColumns #directions").onclick = function() {
-		this.classList.toggle("tpShow");
+		document.querySelector("#titleBanner h1").innerHTML = this.recipeName;
+		document.querySelector("#contributor").innerHTML = this.contributor;
+		document.querySelector("#titleBanner").style.backgroundImage = "url(" + this.imageURL + ")";
+		loadFileInto(this.ingredients, "#ingredients ul" );
+		loadFileInto(this.equipment, "#equipment ul" );
+		loadFileInto(this.directions, "#directions ol" );
+		
 	}
 	
+}
+
+GoodOldfashionedPancakes = new Recipe(
+  "Good Old fashioned Pancakes", "Maddy", "https://images.unsplash.com/photo-1590137876181-2a5a7e340308?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
+  "ingredients.html", 
+	"equipment.html", 
+	"directions.html");
+  
+  MushroomSalsaChili = new Recipe( 
+    "Mushroom Salsa Chili", "sarah hoppe", "https://images.unsplash.com/photo-1608376630927-d064ac74866e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=558&q=80", 
+    "mushroom-ingredients.html",
+"mushroom-equipment.html","mushroom-directions.html");
+  
+WineMustard = new Recipe(
+  "Hal's Wine mustard",
+  "Ally Steigmann",
+  "https://images.unsplash.com/photo-1532641140-28932037521b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "mustard-ingredients.html",
+  "mustard-equipment.html",
+  "mustard-directions.html", );
+  
+  
+window.onload = function() {
 	
+	document.querySelector("#firstRecipe").onclick =function() {
+		GoodOldfashionedPancakes.displayRecipe();
+	}
 
+  
+}
 	
 	
-} // end window.onload*/
-
-
-
